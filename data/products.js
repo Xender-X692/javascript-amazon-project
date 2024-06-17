1c3cca43e3531677aa9fb00fac4ded525e208290
Sitt Naing Htoo <uhlahtoo07@gmail.com>
2024-06-17 20:50:08 +0630

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -75,6 +75,26 @@ logThis();
 logThis.call("hello");
 
 */
+
+export let products = [];
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    fun();
+  });
+  xhr.open("GET", "https://supersimplebackend.dev/products");
+  xhr.send();
+}
+// loadProducts();
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -552,3 +572,5 @@ export const products = [
   }
   return new Product(productDetails);
 });
+
+*/
